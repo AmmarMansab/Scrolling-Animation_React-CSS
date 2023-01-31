27f968595550fcbe55929ce8d9166434daa6d768
AmmarMansab <81436005+AmmarMansab@users.noreply.github.com>
2023-01-31 22:48:55 +0500

    Main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainContainer from './MainContainer';
+
+function App() {
+  return (
+    <div>
+      <MainContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainContainer.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import "./MainContainer.css";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+const MainContainer = () => {
+  useEffect(() => {
+    AOS.init({ duration: 600, easing: "ease", once: false });
+  });
+
+  return (
+    <div className="main_container">
+      <div className="box" data-aos="fade-up" data-aos-delay="100"></div>
+      <div className="box" data-aos="fade-down" data-aos-delay="50"></div>
+      <div className="box" data-aos="fade-down-right" data-aos-delay="20"></div>
+      <div className="box" data-aos="zoom-in-up" data-aos-delay="70"></div>
+      <div className="box" data-aos="slide-right" data-aos-delay="50"></div>
+    </div>
+  );
+};
+
+export default MainContainer;
